8db2b2cdc02d55703b70e1cc288f46bf8893829e
PavanReddy29 <82900072+PavanReddy29@users.noreply.github.com>
2021-04-28 22:11:46 +0530

    Script file

---
+++ dogs.js
@@ -0,0 +1,12 @@
+function CreamLabrador() {
+    var crmlab = "https://www.thelabradorsite.com/wp-content/uploads/2016/01/0001-132020326.jpg";
+    window.open(crmlab);
+}
+function BlackLabrador() {
+    var blklab = "https://www.thelabradorsite.com/wp-content/uploads/2017/04/black-lab-waiting.jpg";
+    window.open(blklab);
+}
+function BrownLabrador() {
+    var brwnlab = "https://www.thelabradorsite.com/wp-content/uploads/2011/11/chocolate-lab.jpg";
+    window.open(brwnlab);
+}
